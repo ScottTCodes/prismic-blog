1529cf6c4a489b3aab42ff05e91bc3c75127ffe7
Scott Taylor <scott1996taylor@outlook.com>
2021-02-22 16:31:16 +0000

    Initial commit

---
+++ prismic-configuration.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The repo name for your Prismic repository (found in the url of your repo: https://prismic-flexible.prismic.io/)
+  prismicRepo: 'prismic-flexible',
+
+  // Release ID to build (requires access)
+  releaseID: '',
+
+  // access token to access the release
+  accessToken: '',
+}
